0d0a2a514ebdadd65bee30f4f5162a408a83e0d4
= <me@rrocha.uk>
2026-06-08 08:54:00 +0100

    chore: improve ast printer

--- src/ast_printer.cpp
+++ src/ast_printer.cpp
@@ -103,12 +103,7 @@ public:
   void visit(RangeExpr &expr) override
   {
     print_indent();
-    std::cout << "RangeExpr(\n";
-    indent++;
-    expr.accept(*this);
-    indent--;
-    print_indent();
-    std::cout << ")\n";
+    std::cout << "RangeExpr(" << expr.start << " to " << expr.end << " by " << expr.step << ")\n";
   }
 
   void visit(NumberExpr &expr) override
@@ -132,12 +127,15 @@ public:
   void visit(BinaryExpr &expr) override
   {
     print_indent();
-    std::cout << "Binary(" << expr.operation.value << ")\n";
-
+    std::cout << "Binary(\n";
     indent++;
     expr.left->accept(*this);
+    print_indent();
+    std::cout << token_type_to_string(expr.operation.type) << "(" << expr.operation.value << ")" << "\n";
     expr.right->accept(*this);
     indent--;
+    print_indent();
+    std::cout << ")\n";
   }
 
   void visit(UnaryExpr &expr) override
@@ -149,8 +147,7 @@ public:
   void visit(ConcatExpr &expr) override
   {
     print_indent();
-    std::cout << "Concat(";
-
+    std::cout << "Concat(\n";
     indent++;
     expr.left->accept(*this);
     expr.right->accept(*this);
@@ -228,8 +225,12 @@ public:
     for (const auto &[field_name, field_expr]: expr.fields)
     {
       print_indent();
-      std::cout << "Identifier(" << field_name << "):";
+      std::cout << "Identifier(" << field_name << "\n";
+      indent++;
       field_expr->accept(*this);
+      indent--;
+      print_indent();
+      std::cout << ")\n";
     }
     indent--;
     print_indent();
